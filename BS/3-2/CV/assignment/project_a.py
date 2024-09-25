7369b05c473d805dbafcbaa7021880c96b64d9a8
andylee <lyandrew1225@gmail.com>
2024-09-26 04:42:19 +0900

    두번째

--- BS/3-2/CV/assignment/project_a.py
+++ BS/3-2/CV/assignment/project_a.py
@@ -14,7 +14,7 @@ B = img[:, :, 0]  # 파란 채널
 
 # 공식 I = 0.299R + 0.587G + 0.114B를 사용하여 흑백 이미지 계산
 cvt_img = 0.299 * R + 0.587 * G + 0.114 * B
-cvt_img = cv2.convertScaleAbs(cvt_img)  # 결과를 uint8 형식으로 변환
+cvt_img = cv2.convertScaleAbs(cvt_img)
 
 # 원본 이미지와 흑백 이미지를 표시
 cv2.imshow("Color Image", img)
